7b334840b800ced42fa75904a9ce0ed7cc07e4f7
two-Bob <wmbbll@163.com>
2022-11-03 11:58:18 +0800

    新增接口方法

--- src/api/blog.js
+++ src/api/blog.js
@@ -23,6 +23,28 @@ export async function getBlogCategories() {
 /**
  * 获取单个博客
  */
-export async function getBlog(params) {
-    
+export async function getBlog(id) {
+	return await request.get(`/api/blog/${id}`)
+}
+
+/**
+ * 提交评论
+ */
+
+export async function postComment(commentInfo) {
+	return await request.post(`/api/comment`, commentInfo)
+}
+
+/**
+ * 获取分页评论
+ */
+
+export async function getComment(blogid, page = 1, limit = 10) {
+	return await request.get(`/api/comment`, {
+		params: {
+			blogid,
+			page,
+			limit,
+		},
+	})
 }
